4f3f3e6ddc92d3caf7551b0b2a79452883efdd49
TruffleTheProto <angel.perez109@upr.edu>
2025-12-07 22:29:20 -0400

    TASK 8 Done

--- levels/LevelManager.py
+++ levels/LevelManager.py
@@ -58,5 +58,8 @@ class LevelManager():
     #   It should check each sublevel in order and return the first one that isn’t finished.
     #   Stop once all have been checked or one is found. Avoid using loops. (USE RECURSIONS)
     def next_unfinished_sublevel(self, index=0):
-        return None
-    
+        if index >= 3:
+            return None
+        if not self.curLevel[index].finished:
+            return self.curLevel[index]
+        return self.next_unfinished_sublevel(index+1)
